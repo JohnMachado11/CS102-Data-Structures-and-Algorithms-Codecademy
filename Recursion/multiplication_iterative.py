4a01b0a79c42c19b732b77c9b9be47eeffae598a
JohnMachado11 <iamjohnmachado@gmail.com>
2022-07-31 23:05:16 -0500

    More code examples

---
+++ Recursion/multiplication_iterative.py
@@ -0,0 +1,14 @@
+
+def multiplication(num_1, num_2):
+    result = 0
+    for count in range(0, num_2):
+        result += num_1
+    return result
+
+
+print(multiplication(3, 7))
+# 21
+print(multiplication(5, 5))
+# 25
+print(multiplication(0, 4))
+# 0
